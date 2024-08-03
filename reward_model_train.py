4a1e97cae4c8f8a5c2e38e1a131faa798241a54e
AsphodelRem <asphodel.bog.rem@gmail.com>
2024-08-03 19:37:31 +0700

    Added loading config from toml

--- reward_model_train.py
+++ reward_model_train.py
@@ -1,48 +1,46 @@
-from datasets import load_dataset
+import argparse
 from transformers import (
     AutoModelForSequenceClassification,
     AutoTokenizer,
-    HfArgumentParser,
 )
 from trl import (
     RewardTrainer,
     RewardConfig,
-    ModelConfig,
-    get_kbit_device_map,
-    get_peft_config,
-    get_quantization_config,
 )
 
-from dataset_utils import get_datasets
+from dataset_utils import get_datasets_for_reward_model
+from config_utils import load_config
+
 
 """
 Usage:
-python3 reward_model_train.py \
-    --model_name_or_path="distilbert/distilbert-base-cased" \
-    --output_dir="d_bert" \
-    --per_device_train_batch_size=64 \
-    --per_device_test_batch_size=64 \
-    --num_train_epochs=1 \
-    --gradient_checkpointing=True \
-    --remove_unused_columns=False \
-    --max_length=512
+    python3 reward_model_train.py --config path/to/config.toml
 """
-
 if __name__ == "__main__":
-    parser = HfArgumentParser((RewardConfig, ModelConfig))
-    reward_config, bert_config = parser.parse_args_into_dataclasses()
-    reward_config.gradient_checkpointing_kwargs = dict(use_reentrant=False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str)
+    args = parser.parse_args()
+    config = load_config(args.config)
+    
+    reward_config = RewardConfig(
+        output_dir=config['reward_model_trainer']['output_dir'],
+        per_device_train_batch_size=config['reward_model_trainer']['per_device_train_batch_size'],
+        num_train_epochs=config['reward_model_trainer']['num_train_epochs'],
+        gradient_checkpointing=config['reward_model_trainer']['gradient_checkpointing'],
+        remove_unused_columns=config['reward_model_trainer']['remove_unused_columns'],
+        max_length=config['reward_model_trainer']['max_length']
+    )
 
     # Create a model
     tokenizer = AutoTokenizer.from_pretrained(
-        bert_config.model_name_or_path, use_fast=True
+        config['reward_model_trainer']['model_name_or_path'], use_fast=True
     )
     model = AutoModelForSequenceClassification.from_pretrained(
-        bert_config.model_name_or_path, num_labels=1
+        config['reward_model_trainer']['model_name_or_path'], num_labels=1
     )
 
     # Get train/test split of the dataset
-    train_dataset, test_dataset = get_datasets(tokenizer)
+    train_dataset, test_dataset = get_datasets_for_reward_model(config, tokenizer)
 
     # Train
     trainer = RewardTrainer(
@@ -51,10 +49,8 @@ if __name__ == "__main__":
         args=reward_config,
         train_dataset=train_dataset,
         eval_dataset=test_dataset,
-        peft_config=get_peft_config(bert_config),
     )
 
     trainer.train()
-
     metrics = trainer.evaluate()
     trainer.log_metrics("eval", metrics)
